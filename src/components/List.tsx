d6c201840f6d2a65a9c84172b7952728e265135f
Fred Hawk <fredrichawk@gmail.com>
2019-07-17 07:50:46 +0200

    fix: Added background to listitems to differentiate and hover.

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -3,7 +3,7 @@ import { ListItem } from "./ListItem";
 
 export function List({ list }) {
   return (
-    <ul>
+    <ul className="list">
       {list.length > 0 ? (
         list.map((listitem: object) => (
           <ListItem key={listitem.id} item={listitem} />
