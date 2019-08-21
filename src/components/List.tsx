ce72b73d0007615870b5692131d9bf4c3ce77e2e
Fred Hawk <fredrichawk@gmail.com>
2019-08-21 07:25:42 +0200

    feat: Added animation for the list.

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,12 +1,21 @@
 import * as React from "react";
+import { useTransition, animated } from "react-spring";
 import { ListItem } from "./ListItem";
 
 export function List({ list }) {
+  const transition = useTransition(list, item => item.imdbID, {
+    from: { opacity: 0, marginLeft: -100, marginRight: 100 },
+    enter: { opacity: 1, marginLeft: 0, marginRight: 0 },
+    config: { duration: 300 }
+  });
+
   return (
     <ul className="list">
-      {list.map(
-        (listitem: object): React.ReactElement => (
-          <ListItem key={listitem.imdbID} item={listitem} />
+      {transition.map(
+        ({ item, props, key }): React.ReactElement => (
+          <animated.div key={key} style={props}>
+            <ListItem item={item} />
+          </animated.div>
         )
       )}
     </ul>
